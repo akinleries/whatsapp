2eec4b81a21125a3757a231c6e7d0d397db12c5f
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2023-05-05 23:38:26 +0100

    backend Authentication

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatsScreen from './src/screens/ChatsScreen';
-import ChatScreen from './src/screens/ChatScreen';
-import Message from "./src/components/message";
 import Navigator from './src/navigation';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { withAuthenticator } from "aws-amplify-react-native";
+
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } })
 
 // const chat = {
 //   id: "1",
@@ -18,13 +20,13 @@ import Navigator from './src/navigation';
 //   },
 // };
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
 
       <Navigator />
-     
-   
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -35,6 +37,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'whitesmoke',
     justifyContent: 'center',
-   
+
   },
 });
+
+export default withAuthenticator(App);
