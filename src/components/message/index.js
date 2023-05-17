d68ab150aae9b1026184107095c69d4acce0ac09
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2023-05-17 11:13:55 +0100

    worked on the view image in the chatscreen

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,15 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { Auth } from "aws-amplify";
-import { S3Image } from "aws-amplify-react-native";
+import { Auth, Storage } from "aws-amplify";
+//import { S3Image } from "aws-amplify-react-native";
+import ImageView from "react-native-image-viewing";
+ 
 
 dayjs.extend(relativeTime);
 
 const Message = ({ message }) => {
 
     const [isMe, setIsMe] = useState(false);
+    const [imageSources, setImageSources] = useState([]);
+    const [imageViewerVisible, setImageViewerVisible] = useState(false);
 
     const isMyMessage = async () => {
 
@@ -23,9 +27,34 @@ const Message = ({ message }) => {
     }, [])
 
 
+    useEffect(() => {
+        const downloadImages = async () => {
+            if (message.images.length > 0){
+                const uri = await Storage.get(message.images[0]);
+
+               setImageSources([{ uri }])
+            }
+        }
+
+        downloadImages();
+    },[message.images])
+
+
     return (
         <View style={[styles.container, { backgroundColor: isMe ? '#DCF8C5' : 'white', alignSelf: isMe ? 'flex-end' : 'flex-start' }]}>
-            {message.images?.length > 0 && <S3Image  imgKey={message.images[0]} style={styles.image}/>}
+            {message.images?.length > 0 && (
+                <>
+                   <Pressable onPress={() => setImageViewerVisible(true)}>
+                        <Image source={imageSources[0]} style={styles.image} />
+                   </Pressable>
+                    <ImageView 
+                        images={imageSources} 
+                        imageIndex={0} 
+                        visible={imageViewerVisible} 
+                        onRequestClose={() => setImageViewerVisible(false)}
+                    />
+                </>
+            )}
             <Text>{message.text}</Text>
             <Text style={styles.time}>{dayjs(message.createdAt).fromNow(true)}</Text>
         </View>
